b5b500b2ebba7c735a10522dbfd9b8613d659f8b
NiteshMalknm <84245005+NiteshMalknm@users.noreply.github.com>
2023-07-25 23:52:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from telethon.sync import TelegramClient,events
+import os
+
+if os.path.isfile('app.session'):
+    os.remove("app.session")
+
+api_id = 21830791
+api_hash = '041e0a1089a469a80ca4cc5d9c46196a'
+bot_token = '5918508248:AAHQeXJGa_UbhwqCHs7RH8jGniGIHCBeGAM'
+
+
+client = TelegramClient('app', api_id, api_hash)
+
+
+@client.on(events.NewMessage(outgoing=True, pattern=r'\/audio'))
+async def handler(event):
+    print(event)
+    
+with client:
+    client.start()
+    client.run_until_disconnected()

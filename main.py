361620845b57ed5359e9bb6861a9e0dd8134dd79
NiteshMalknm <84245005+NiteshMalknm@users.noreply.github.com>
2023-07-26 00:08:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from telethon.sync import TelegramClient,events
-import os
 
-if os.path.isfile('app.session'):
-    os.remove("app.session")
 
 api_id = 21830791
 api_hash = '041e0a1089a469a80ca4cc5d9c46196a'
